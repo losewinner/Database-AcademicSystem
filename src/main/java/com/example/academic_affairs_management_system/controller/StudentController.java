f4d33a488cd1050604395f42575b2df882efb4a4
losewinner <1541145418@qq.com>
2023-04-27 19:05:19 +0800

    初始

--- src/main/java/com/example/academic_affairs_management_system/controller/StudentController.java
+++ src/main/java/com/example/academic_affairs_management_system/controller/StudentController.java
@@ -1,10 +1,14 @@
 package com.example.academic_affairs_management_system.controller;
 
 
+import com.example.academic_affairs_management_system.service.IStudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
-
+import com.example.academic_affairs_management_system.entity.Student;
+import java.util.List;
 /**
  * <p>
  *  前端控制器
@@ -16,5 +20,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/student")
 public class StudentController {
+    @Autowired
+    private IStudentService iStudentService;
 
+    @GetMapping("/list")
+    public List<Student>list(){
+        return iStudentService.list();
+    }
 }
