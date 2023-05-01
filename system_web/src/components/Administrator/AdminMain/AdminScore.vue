ae20ade442631de87e5d4d24f7b25df4de20fdd9
losewinner <1541145418@qq.com>
2023-05-01 15:21:00 +0800

    系统管理员【统计成绩功能】前端界面

--- system_web/src/components/Administrator/AdminMain/AdminScore.vue
+++ system_web/src/components/Administrator/AdminMain/AdminScore.vue
@@ -6,17 +6,69 @@
             <el-input placeholder="请输入课程号" suffix-icon="el-icon-s-management" v-model="input.courseId" style="width:30% ;margin-right: 5px"></el-input>
             <el-input placeholder="请输入课程名字" suffix-icon="el-icon-s-management" v-model="input.courseName" style="width:30% ;margin-right: 5px"></el-input>
         </el-container>
-        <el-container class="学生号，学生姓名" style="margin-top: 20px">
+        <el-container class="学生号，学生姓名" style="margin-top: 10px">
             <el-input placeholder="请输入学生号" suffix-icon="el-icon-user" v-model="input.studentId" style="width:30%;margin-right: 5px"></el-input>
             <el-input placeholder="请输入学生姓名" suffix-icon="el-icon-user" v-model="input.studentName" style="width:30% ;margin-right: 5px"></el-input>
         </el-container>
-        <el-container class="教师号，教师姓名" style="margin-top: 20px">
+        <el-container class="教师号，教师姓名" style="margin-top:10px">
             <el-input placeholder="请输入教师号" suffix-icon="el-icon-s-custom" v-model="input.staffId" style="width:30%;margin-right: 5px"></el-input>
             <el-input placeholder="请输入教师姓名" suffix-icon="el-icon-s-custom" v-model="input.staffName" style="width:30% ;margin-right: 5px"></el-input>
         </el-container>
+        <el-container class="查找按钮" style="margin-top: 20px">
+            <el-button @click="searchClick()" type="primary" icon="el-icon-search">搜索</el-button>
+        </el-container>
     </el-container>
     <el-container class="查找结果表格" style="margin-top: 20px">
-        <div>表格</div>
+        <el-table
+            :data="FromDbInfo"
+            style="width: 70%"
+            max-height="250">
+            <el-table-column
+                fixed
+                prop="semester"
+                label="学期"
+                width="150">
+            </el-table-column>
+            <el-table-column
+                prop="studentId"
+                label="学号"
+                width="150">
+            </el-table-column>
+            <el-table-column
+                prop="studentName"
+                label="学生姓名"
+                width="150">
+            </el-table-column>
+            <el-table-column
+                prop="staffId"
+                label="工号"
+                width="150">
+            </el-table-column>
+            <el-table-column
+                prop="staffName"
+                label="老师姓名"
+                width="150">
+            </el-table-column>
+            <el-table-column
+                prop="score"
+                label="绩点"
+                width="120">
+            </el-table-column>
+            <el-table-column
+                fixed="right"
+                label="操作"
+                width="170">
+                <template slot-scope="scope">
+                    <el-button  @click="handleClick(scope.row)" type="text" size="medium">编辑</el-button>
+                    <el-button
+                        @click.native.prevent="deleteRow(scope.$index, FromDbInfo)"
+                        type="text"
+                        size="medium" style="color:red">
+                        移除
+                    </el-button>
+                </template>
+            </el-table-column>
+        </el-table>
     </el-container>
 </el-container>
 
@@ -37,6 +89,27 @@ export default {
             staffId:'',
             staffName:''
             },
+            FromDbInfo:[{
+                semester: '2023春季',
+                studentId: '12345',
+                studentName:'陆靖宇',
+                courseId: '12345',
+                courseName:'BB88',
+                staffId: '123',
+                staffName:'lfy',
+                score: 2003
+            }]
+        }
+    },
+    methods:{
+        deleteRow(index,rows){
+            rows.splice(index,1);
+        },
+        handleClick(row){
+            console.log(row)
+        },
+        searchClick(){
+            console.log("wtf")
         }
     }
 }
